4c9d0dd2b9a3a878dd56256db66766025c6acc61
CesarVazquezTellez <cesarvazquezt00@gmail.com>
2025-11-26 12:21:27 -0600

    02 Aplicacion correcta de styles con pagina index

--- djangostore/urls.py
+++ djangostore/urls.py
@@ -22,8 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    # path('products/', include('products.urls')),
+    # path('accounts/', include()),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
